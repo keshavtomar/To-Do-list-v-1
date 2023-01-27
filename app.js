c6bb45b713376754b149aa530f3e032226feb802
keshav <tomarkeshavofficial@gmail.com>
2023-01-28 00:40:49 +0530

    extra lines removed

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const day = require(__dirname+"/date.js");
+const day = require(__dirname + "/date.js");
 
 const app = express();
 app.set("view engine", "ejs");
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
 app.use(express.static("public"));
@@ -16,60 +16,49 @@ app.use(express.static("public"));
 let Items = [];
 let workItems = [];
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
+    let dayname = day();
 
-  let dayname = day();
-
-  res.render("list", {
-    listtitle: dayname,
-    Itemspassed: Items,
-  });
+    res.render("list", {
+        listtitle: dayname,
+        Itemspassed: Items,
+    });
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", {
-    listtitle: "Work",
-    Itemspassed: workItems,
-  });
+app.get("/work", function(req, res) {
+    res.render("list", {
+        listtitle: "Work",
+        Itemspassed: workItems,
+    });
 });
 
-app.get("/about", function(req,res){
-  res.render("about");
-})
-
-app.post("/", function (req, res) {
-  let Item = req.body.newItem;
-
-  if (req.body.listvalue === "Work") {
-    workItems.push(Item);
-    res.redirect("work");
-  } else {
-    Items.push(Item);
-    res.redirect("/");
-  }
-
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
-app.post("/work", function (req, res) {
-  let Item = req.body.newItem;
-  workItems.pust(Item);
-  res.redirect("/work");
+app.post("/", function(req, res) {
+    let Item = req.body.newItem;
+
+    if (req.body.listvalue === "Work") {
+        workItems.push(Item);
+        res.redirect("work");
+    } else {
+        Items.push(Item);
+        res.redirect("/");
+    }
 });
 
-app.post("/delete", function (req, res) {
-  // console.log(req.body);
-   if(req.body.currentlist === "Work"){
-     workItems = [];
-     res.redirect("/work");
-   }
-   else{
-     Items=[];
-     res.redirect("/");
-   }
-
+app.post("/delete", function(req, res) {
+    // console.log(req.body);
+    if (req.body.currentlist === "Work") {
+        workItems = [];
+        res.redirect("/work");
+    } else {
+        Items = [];
+        res.redirect("/");
+    }
 });
 
-
-app.listen("3000", function () {
-  console.log("Server started at port : 3000");
+app.listen("3000", function() {
+    console.log("Server started at port : 3000");
 });
